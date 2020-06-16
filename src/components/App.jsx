3bb304131798fc5d08d2de7f5f6620eafc298073
LnkColumbus <marlonveliz.nero19@gmail.com>
2020-06-16 11:20:53 -0600

    Estilos al proyecto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hola</div>;
+  const rows = () => [
+    <tr>
+      <td>Marlon</td>
+      <td>marlonveliz.nero19@gmail.com</td>
+      <td>platzi.com</td>
+    </tr>,
+
+    <tr>
+      <td>Platzi</td>
+      <td>platzi@platzi.com</td>
+      <td>platzi.com</td>
+    </tr>,
+  ];
+
+  return (
+    <div className="margen">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>{rows()}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default App;

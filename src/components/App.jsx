73bda53d84424a7ffb6416ffc93d9ded5b6e4f0c
LnkColumbus <marlonveliz.nero19@gmail.com>
2020-06-16 19:29:34 -0600

    Agregando redux al proyecto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Menu from "./Menu";
+import Users from "./Users";
+
+const Tareas = () => <div>Tareas</div>;
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    setUsers([
-      {
-        id: 1,
-        name: "Marlon",
-        email: "marlonveliz.nero19@gmail.com",
-        website: "marlon.com",
-      },
-      {
-        id: 2,
-        name: "Platzi",
-        email: "platzi@platzi.com",
-        website: "platzi.com",
-      },
-    ]);
-  }, []);
-
-  const rows = () =>
-    users.map((user) => (
-      <tr key={user.id}>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>{user.website}</td>
-      </tr>
-    ));
-
   return (
-    <div className="margen">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>{rows()}</tbody>
-      </table>
-    </div>
+    <BrowserRouter>
+      <Menu />
+      <div className="margen">
+        <Route exact path="/" component={Users} />
+        <Route exact path="/tareas" component={Tareas} />
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Users/index.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+
+const Users = (props) => {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(response.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const rows = () =>
+    users.map((user) => (
+      <tr key={user.id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.website}</td>
+      </tr>
+    ));
+
+  console.log(props);
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>{rows()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+const mapStateToProps = (reducers) => {
+  return reducers.usersReducer;
+};
+
+export default connect(mapStateToProps, {
+  /*Actions*/
+})(Users);

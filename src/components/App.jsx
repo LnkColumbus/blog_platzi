dad75471ab415b0bee694b01effb18d1ef906dcb
LnkColumbus <marlonveliz.nero19@gmail.com>
2020-06-19 11:21:30 -0600

    Agregando las publicaciones del usuario al proyecto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Menu from "./Menu";
 import Users from "./Users";
+import Posts from "./Posts";
 
 const Tareas = () => <div>Tareas</div>;
 
@@ -12,6 +13,7 @@ const App = () => {
       <div className="margen">
         <Route exact path="/" component={Users} />
         <Route exact path="/tareas" component={Tareas} />
+        <Route exact path="/posts/:key" component={Posts} />
       </div>
     </BrowserRouter>
   );
--- src/components/Users/Table.jsx
+++ src/components/Users/Table.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 const Table = (props) => {
   const { users } = props;
   const rows = () =>
-    users.map((user) => (
+    users.map((user, key) => (
       <tr key={user.id}>
         <td>{user.name}</td>
         <td>{user.email}</td>
         <td>{user.website}</td>
+        <td>
+          <Link to={`/posts/${key}`}>
+            <div className="eye-solid2 icon"></div>
+          </Link>
+        </td>
       </tr>
     ));
   return (
